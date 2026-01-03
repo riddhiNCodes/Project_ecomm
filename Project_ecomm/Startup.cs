1ee91a4ab5b3e38d78e7ef3c082bc2447cefc5c8
riddhiNCodes <riddhincodes@gmail.com>
2026-01-03 13:00:39 +0530

    Add project files.

---
+++ Project_ecomm/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Project_ecomm.Startup))]
+namespace Project_ecomm
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
