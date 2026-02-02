62583d0127cf3f9c01c0f6ffb1e6734366c64bfe
riddhiNCodes <riddhincodes@gmail.com>
2026-02-02 20:21:27 +0530

    with db

---
+++ Project_ecomm/Migrations/202602021428500_InitialCreate.cs
@@ -0,0 +1,16 @@
+﻿namespace Project_ecomm.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
