8e882940f54aa99ed77e4258e06837c3e295b6b0
riddhiNCodes <riddhincodes@gmail.com>
2026-02-03 12:39:19 +0530

    loginregister

--- Project_ecomm/Models/IdentityModels.cs
+++ Project_ecomm/Models/IdentityModels.cs
@@ -9,11 +9,16 @@ namespace Project_ecomm.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
-        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+       
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(
+            UserManager<ApplicationUser> manager)
         {
-            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
-            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
-            // Add custom user claims here
+            var userIdentity = await manager.CreateIdentityAsync(
+                this,
+                DefaultAuthenticationTypes.ApplicationCookie
+            );
+
             return userIdentity;
         }
     }
@@ -30,4 +35,5 @@ namespace Project_ecomm.Models
             return new ApplicationDbContext();
         }
     }
+
 }
